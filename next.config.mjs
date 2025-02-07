f26aac23e8b4f7f5a1f515f1114bb6af8968198a
Ahamed Izzard <ahamadizzard@hotmail.com>
2025-02-07 17:51:43 +0530

    adjusted the package JSON

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.media-amazon.com",
+        port: "",
+        search: "",
+      },
+      {
+        protocol: "https",
+        hostname: "**.themoviedb.org",
+        port: "",
+        search: "",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
